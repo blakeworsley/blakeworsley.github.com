3abbcb315906da7a6e1d6180735cbd16db0fc40c
Blake Worsley <blakeworsley@gmail.com>
2016-11-16 16:13:19 -0700

    hidden projects until mouseover

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,25 +5,25 @@ class Projects extends Component {
   render() {
     return (
       <section className='Projects'>
-        <article className='project-container row-1'>Project 1</article>
-        <article className='project-container row-1'>Project 1.2</article>
-        <article className='project-container row-1'>Project 1.3</article>
-        <article className='project-container row-1'>Project 1.4</article>
+        <article className='project-container row-1'><div className='project'>Project 1</div></article>
+        <article className='project-container row-1'><div className='project'>Project 1.2</div></article>
+        <article className='project-container row-1'><div className='project'>Project 1.3</div></article>
+        <article className='project-container row-1'><div className='project'>Project 1.4</div></article>
 
-        <article className='project-container row-2'>Project 2</article>
-        <article className='project-container row-2'>Project 2.2</article>
-        <article className='project-container row-2'>Project 2.3</article>
-        <article className='project-container row-2'>Project 2.4</article>
+        <article className='project-container row-2'><div className='project'>Project 2</div></article>
+        <article className='project-container row-2'><div className='project'>Project 2.2</div></article>
+        <article className='project-container row-2'><div className='project'>Project 2.3</div></article>
+        <article className='project-container row-2'><div className='project'>Project 2.4</div></article>
 
-        <article className='project-container row-3'>Project 3</article>
-        <article className='project-container row-3'>Project 3.2</article>
-        <article className='project-container row-3'>Project 3.3</article>
-        <article className='project-container row-3'>Project 3.4</article>
+        <article className='project-container row-3'><div className='project'>Project 3</div></article>
+        <article className='project-container row-3'><div className='project'>Project 3.2</div></article>
+        <article className='project-container row-3'><div className='project'>Project 3.3</div></article>
+        <article className='project-container row-3'><div className='project'>Project 3.4</div></article>
 
-        <article className='project-container row-4'>Project 4</article>
-        <article className='project-container row-4'>Project 4.2</article>
-        <article className='project-container row-4'>Project 4.3</article>
-        <article className='project-container row-4'>Project 4.4</article>
+        <article className='project-container row-4'><div className='project'>Project 4</div></article>
+        <article className='project-container row-4'><div className='project'>Project 4.2</div></article>
+        <article className='project-container row-4'><div className='project'>Project 4.3</div></article>
+        <article className='project-container row-4'><div className='project'>Project 4.4</div></article>
       </section>
     );
   }
