16c2df9b56d8dd076299dd2926ed845ac6394fb3
Blake Worsley <blakeworsley@gmail.com>
2016-11-20 22:23:42 -0700

    added styling

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
-import './css/reset.css';
-import './css/index.css';
+import './styles/css/index.css';
 
 ReactDOM.render(
   <App />,
