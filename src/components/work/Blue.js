ef05fc03e526c0eb8ab76eb60b3fb412c6f6155d
Blake Worsley <blakeworsley@gmail.com>
2016-12-15 09:39:31 -0700

    adds photos

--- src/components/work/Blue.js
+++ src/components/work/Blue.js
@@ -4,6 +4,7 @@ class Blue extends Component {
   render() {
     return (
       <section className='computer-application'>
+        <div className='project-screenshot'></div>      
         <h4 className='project-header'>Blue Coach</h4>
         <p className='project-content'>
           Designed to help swimmers communicate with their Coaches to
