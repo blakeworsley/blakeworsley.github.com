e35baf3772f2c4fe70e3fb10eb0693a0e2b37508
Blake Worsley <blakeworsley@gmail.com>
2016-12-17 16:25:39 -0700

    adds hover centering in about section

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -7,22 +7,22 @@ class Tiles extends Component {
         <article className='tile-container tile-1'><div className='tile'>Swimmer</div></article>
         <article className='tile-container tile-1'><div className='tile'>Developer</div></article>
         <article className='tile-container tile-1'><div className='tile'>Artist</div></article>
-        <article className='tile-container tile-1'><div className='tile'>Adventurer</div></article>
+        <article className='tile-container tile-1'><div className='tile'>Olympian</div></article>
 
-        <article className='tile-container tile-2'><div className='tile'>Denver</div></article>
-        <article className='tile-container tile-2'><div className='tile'>Steamboat Springs</div></article>
-        <article className='tile-container tile-2'><div className='tile'>Vancouver</div></article>
-        <article className='tile-container tile-2'><div className='tile'>Toronto</div></article>
+        <article className='tile-container tile-2'><div className='tile'>Denverite</div></article>
+        <article className='tile-container tile-2'><div className='tile'>Steamboatian</div></article>
+        <article className='tile-container tile-2'><div className='tile'>Vancouverman</div></article>
+        <article className='tile-container tile-2'><div className='tile'>Torontonian</div></article>
 
-        <article className='tile-container tile-3'><div className='tile'>Skiing</div></article>
-        <article className='tile-container tile-3'><div className='tile'>Gladiator</div></article>
-        <article className='tile-container tile-3'><div className='tile'>Walking Dead</div></article>
-        <article className='tile-container tile-3'><div className='tile'>Westworld</div></article>
+        <article className='tile-container tile-3'><div className='tile'>Skier</div></article>
+        <article className='tile-container tile-3'><div className='tile'>Designer</div></article>
+        <article className='tile-container tile-3'><div className='tile'>Photoshoper</div></article>
+        <article className='tile-container tile-3'><div className='tile'>Incliner</div></article>
 
         <article className='tile-container tile-4'><div className='tile'>The Incline</div></article>
-        <article className='tile-container tile-4'><div className='tile'>Olympian</div></article>
-        <article className='tile-container tile-4'><div className='tile'>Gym</div></article>
-        <article className='tile-container tile-4'><div className='tile'>Fresh Air</div></article>
+        <article className='tile-container tile-4'><div className='tile'>Adventurer</div></article>
+        <article className='tile-container tile-4'><div className='tile'>Athlete</div></article>
+        <article className='tile-container tile-4'><div className='tile'>Canadian</div></article>
       </section>
     );
   }
